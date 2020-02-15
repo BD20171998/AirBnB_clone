8ef5d054c579ef35e6a3294c7f3e5e9310405a0c
kevapostol <apostolkevin@gmail.com>
2020-02-15 00:35:18 +0000

    Converted uuid to string when we create a new instance

--- models/base_model.py
+++ models/base_model.py
@@ -11,7 +11,7 @@ class BaseModel:
 
     def __init__(self):
         '''Initializes an instance'''
-        self.id = uuid.uuid4()
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
