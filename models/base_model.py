ea838ed4fb57686d0ba4770e8f2a459a519e50e7
kevapostol <apostolkevin@gmail.com>
2020-02-15 04:47:27 +0000

    Base model class update

--- models/base_model.py
+++ models/base_model.py
@@ -9,15 +9,30 @@ import uuid
 class BaseModel:
     '''A BaseModel Module'''
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''Initializes an instance'''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if len(kwargs) > 0:
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+            if 'created_at' in kwargs:
+                self.created_at = datetime.strptime(
+                    kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            if 'updated_at' in kwargs:
+                self.updated_at = datetime.strptime(
+                    kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            if 'my_number' in kwargs:
+                self.my_number = kwargs['my_number']
+            if 'name' in kwargs:
+                self.name = kwargs['name']
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
-        return '{} {} {}'.format(self.__class__.__name__,
-                                 self.id, self.__dict__)
+        return '[{}] ({}) {}'.format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
