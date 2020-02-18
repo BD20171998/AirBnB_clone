85f080fa2f9dc199c95e9244cef7ee0fa6ff2e26
kevapostol <apostolkevin@gmail.com>
2020-02-18 23:12:50 +0000

    The user class

---
+++ models/user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+The User module
+'''
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    '''
+    A User class that inherits BaseModel
+    '''
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
+
+    def __init__(self, *args, **kwargs):
+        '''Initializes an instance'''
+        super().__init__(*args, **kwargs): w
