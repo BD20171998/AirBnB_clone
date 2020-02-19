ad00bfd4e7625a65b8b0afd74fcee5f4200803b8
kevapostol <apostolkevin@gmail.com>
2020-02-19 00:49:56 +0000

    Added modules to import

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,11 @@ The File Storage Module
 '''
 from models.base_model import BaseModel
 from models.user import User
+from models.amenity import Amenity
+from models.state import State
+from models.place import Place
+from models.city import City
+from models.review import Review
 import json
 
 class FileStorage:
