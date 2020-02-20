ec5134ac750fbc30700e7d75fc5258ea099968e4
Bob Deprizio <robert.deprizio@gmail.com>
2020-02-20 00:41:37 +0000

    Added separate test files for classes

---
+++ tests/test_models/test_place.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Unittest for BaseModel class
+"""
+import unittest
+from io import StringIO
+from datetime import datetime
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+class TestBase(unittest.TestCase):
+
+
+    def test_id_diff(self):
+        b1 = BaseModel()
+        b2 = User()
+        self.assertNotEqual(b1.id, b2.id)
+
+    def test_id_None(self):
+        b1 = BaseModel()
+        b2 = State()
+        self.assertIsNotNone(b1.id)
+        self.assertIsNotNone(b2.id)
+
+    def test_place_default(self):
+        b1 = Place()
+
+        self.assertEqual(b1.city_id, '')
+        self.assertEqual(b1.user_id, '')
+        self.assertEqual(b1.name, '')
+        self.assertEqual(b1.description, '')
+        self.assertEqual(b1.number_rooms, 0)
+        self.assertEqual(b1.number_bathrooms, 0)
+        self.assertEqual(b1.max_guest, 0)
+        self.assertEqual(b1.price_by_night, 0)
+        self.assertEqual(b1.latitude, 0.0)
+        self.assertEqual(b1.longitude, 0.0)
+        self.assertEqual(b1.amenity_ids, [])
