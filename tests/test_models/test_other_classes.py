eb25d56c499b0082c044a394ef31c58fdb64311d
Bob Deprizio <robert.deprizio@gmail.com>
2020-02-20 00:29:10 +0000

    Added tests for 'Place' class

--- tests/test_models/test_other_classes.py
+++ tests/test_models/test_other_classes.py
@@ -39,3 +39,18 @@ class TestBase(unittest.TestCase):
         self.assertEqual(my_user.last_name, "Holberton")
         self.assertEqual(my_user.email, "airbnb@holbertonshool.com")
         self.assertEqual(my_user.password, "root")
+
+    def test_place_default(self):
+        b1 = Place()
+
+        self.assertEqual(b1.city_id, '')
+        self.assertEqual(b1.user_id, '')
+        self.assertEqual(b1.name, '')
+        self.assertEqual(b1.description, '')
+        self.assertEqual(b1.number_rooms, 0)
+        self.assertEqual(b1.number_bathrooms, 0)
+        self.assertEqual(b1.max_guest, 0)
+        self.assertEqual(b1.price_by_night, 0)
+        self.assertEqual(b1.latitude, 0.0)
+        self.assertEqual(b1.longitude, 0.0)
+        self.assertEqual(b1.amenity_ids, [])
