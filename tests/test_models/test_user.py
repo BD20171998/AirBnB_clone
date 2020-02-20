2ff240e9b4c2fe56a3acfc041a7e0c6014893922
Bob Deprizio <robert.deprizio@gmail.com>
2020-02-20 02:39:03 +0000

    Added more tests for 'user' class

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Unittest for BaseModel class
+Unittest for User class
 """
 import unittest
 from io import StringIO
@@ -8,47 +8,51 @@ from datetime import datetime
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+
 
 class TestBase(unittest.TestCase):
 
+    def setUp(self):
+        self.b1 = BaseModel()
+        self.b2 = User()
 
     def test_id_diff(self):
-        b1 = BaseModel()
-        b2 = User()
-        self.assertNotEqual(b1.id, b2.id)
+        self.assertNotEqual(self.b1.id, self.b2.id)
 
     def test_id_None(self):
-        b1 = BaseModel()
-        b2 = State()
         self.assertIsNotNone(b1.id)
         self.assertIsNotNone(b2.id)
 
-    def test_user_default(self):
-        my_user = User()
-        my_user.first_name = "Betty"
-        my_user.last_name = "Holberton"
-        my_user.email = "airbnb@holbertonshool.com"
-        my_user.password = "root"
+    def test_user_attr(self):
+        self.b2.first_name = "Betty"
+        self.b2.last_name = "Holberton"
+        self.b2.email = "airbnb@holbertonshool.com"
+        self.b2.password = "root"
 
-        self.assertEqual(my_user.first_name, "Betty")
-        self.assertEqual(my_user.last_name, "Holberton")
-        self.assertEqual(my_user.email, "airbnb@holbertonshool.com")
-        self.assertEqual(my_user.password, "root")
+        self.assertEqual(self.b2.first_name, "Betty")
+        self.assertEqual(self.b2.last_name, "Holberton")
+        self.assertEqual(self.b2.email, "airbnb@holbertonshool.com")
+        self.assertEqual(self.b2.password, "root")
 
     def test_user_types(self):
+        self.b2.first_name = "Betty"
+        self.b2.last_name = "Holberton"
+        self.b2.email = "airbnb@holbertonshool.com"
+        self.b2.password = "root"
 
-        my_user = User()
-        my_user.first_name = "Betty"
-        my_user.last_name = "Holberton"
-        my_user.email = "airbnb@holbertonshool.com"
-        my_user.password = "root"
+        self.assertEqual(type(self.b2.first_name), str)
+        self.assertEqual(type(self.b2..last_name), str)
+        self.assertEqual(type(self.b2.email), str)
+        self.assertEqual(type(self.b2.password), str)
 
-        self.assertEqual(type(my_user.first_name), str)
-        self.assertEqual(type(my_user.last_name), str)
-        self.assertEqual(type(my_user.email), str)
-        self.assertEqual(type(my_user.password), str)
+    def test_str(self):
+        self.b2.first_name = "Roger"
+        teststr = "[City] ({}) {}".format(self.b2.id, self.b2.__dict__)
+        self.assertEqual(teststr, str(self.b2))
+
+    def test_update_format(self):
+        self.b2.save()
+        b1_json = self.b2.to_dict()
+        u_date_obj = self.b2.updated_at
+        u_date_obj2 = datetime.strptime(b1_json["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+        self.assertEqual(u_date_obj, u_date_obj2)
