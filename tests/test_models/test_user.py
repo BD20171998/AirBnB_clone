cde3b21afcc982cc38ece5d30c539fb60fda10c9
Bob Deprizio <robert.deprizio@gmail.com>
2020-02-26 04:52:57 +0000

    Rewrote tests for 'user' class

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -12,28 +12,23 @@ from models.user import User
 
 class TestBase(unittest.TestCase):
 
-    def setUp(self):
-        self.b1 = BaseModel()
-        self.b2 = User()
+    def test_user_type(self):
+        b1 = User()
+        self.assertEqual(type(b1), User)
 
-    def test_id_diff(self):
-        self.assertNotEqual(self.b1.id, self.b2.id)
+    def test_user_attrs(self):
+        b3 = User()
+        b3.first_name = "Betty"
+        b3.last_name = "Holberton"
+        b3.email = "airbnb@holbertonshool.com"
+        b3.password = "root"
 
-    def test_user_types(self):
-        self.b3 = User()
-        self.b3.first_name = "Betty"
-        self.b3.last_name = "Holberton"
-        self.b3.email = "airbnb@holbertonshool.com"
-        self.b3.password = "root"
+        self.assertEqual(type(b3.first_name), str)
+        self.assertEqual(type(b3.last_name), str)
+        self.assertEqual(type(b3.email), str)
+        self.assertEqual(type(b3.password), str)
 
-        self.assertEqual(type(self.b3.first_name), str)
-        self.assertEqual(type(self.b3.last_name), str)
-        self.assertEqual(type(self.b3.email), str)
-        self.assertEqual(type(self.b3.password), str)
-
-    def test_update_format(self):
-        self.b2.save()
-        b1_json = self.b2.to_dict()
-        u_date_obj = self.b2.updated_at
-        u_date_obj2 = datetime.strptime(b1_json["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
-        self.assertEqual(u_date_obj, u_date_obj2)
+        self.assertEqual(b3.first_name, "Betty")
+        self.assertEqual(b3.last_name, "Holberton")
+        self.assertEqual(b3.email, "airbnb@holbertonschool.com")
+        self.assertEqual(b3.password, "root")
